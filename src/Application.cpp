b973a7098ef0e8e8b9bfde1c8fbceb4e7d3aaee5
Jean <jeanvitordepaulo@gmail.com>
2017-10-08 17:30:17 -0300

    Updated for Vertex Buffers and Drawing a Triangle in OpenGL video

--- src/Application.cpp
+++ src/Application.cpp
@@ -29,16 +29,24 @@ int main(void)
     	cout<<glGetString(GL_VERSION)<<endl;
     }
 
+    float positions[6]={-0.5f,-0.5f,0.0f,0.5f,0.5f,-0.5f};
+
+    unsigned int buffer;
+    glGenBuffers(1,&buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER,6*sizeof(float), positions, GL_STATIC_DRAW);
+
+
+
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-        glBegin(GL_TRIANGLES);
-        glVertex2d(-0.5f,-0.5f);
-        glVertex2d(0.0f,0.5f);
-        glVertex2d(0.5f,-0.5f);
-        glEnd();
+
+
+        glDrawArrays(GL_TRIANGLES,0,3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
