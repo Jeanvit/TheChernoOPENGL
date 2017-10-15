cdd0e0bc9b3b350b061a52224e2922528f8f386e
Jean <jeanvitordepaulo@gmail.com>
2017-10-15 16:01:09 -0200

    Code from Vertex Attributes and Layouts in OpenGL video

--- src/Application.cpp
+++ src/Application.cpp
@@ -36,7 +36,10 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER,6*sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0,2,GL_FLOAT,GL_FALSE, sizeof(float)*2,0);
 
+    glBindBuffer(GL_ARRAY_BUFFER,0);
 
 
     /* Loop until the user closes the window */
