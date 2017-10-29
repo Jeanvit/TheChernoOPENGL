4d2d3dc4b5c42e022a846f30aa5c97076f77b40d
Jean <jeanvitordepaulo@gmail.com>
2017-10-29 15:05:49 -0200

    code for Writing a Shader in OpenGL video

--- src/Application.cpp
+++ src/Application.cpp
@@ -5,6 +5,44 @@
 using std::cout;
 using std::endl;
 
+static unsigned int compileShader (unsigned int type, const std::string& source){
+	 unsigned int id = glCreateShader(type);
+	 const char *src = source.c_str();
+	 glShaderSource(id,1,&src,nullptr);
+	 glCompileShader(id);
+
+	 int result;
+	 glGetShaderiv(id, GL_COMPILE_STATUS, &result);
+	 if (result == GL_FALSE){
+		 int length;
+		 glGetShaderiv(id, GL_INFO_LOG_LENGTH, &length);
+		 char message[length];
+		 glGetShaderInfoLog(id, length, &length,message);
+		 cout<<"Failed to compile" << (type==GL_VERTEX_SHADER ? "vertex" : "fragment") <<"shader"<<endl;
+		 cout<<message<<endl;
+		 glDeleteShader(id);
+		 return 0;
+	 }
+
+	 return id;
+}
+
+static unsigned int createShader(const std::string& vertexShader, const std::string& fragmentShader){
+	unsigned int program = glCreateProgram();
+	unsigned int vs = compileShader(GL_VERTEX_SHADER,vertexShader);
+	unsigned int fs = compileShader(GL_FRAGMENT_SHADER,fragmentShader);
+
+	glAttachShader(program, vs);
+	glAttachShader(program, fs);
+	glLinkProgram(program);
+	glValidateProgram(program);
+
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return program;
+}
+
 int main(void)
 {
     GLFWwindow *window;
@@ -42,6 +80,27 @@ int main(void)
     glBindBuffer(GL_ARRAY_BUFFER,0);
 
 
+    std::string vertexShader =
+    		"#version 330 core \n"
+    		"\n"
+    		"layout(location=0) in vec4 position;"
+    		"\n"
+    		"void main()\n"
+    		"{\n"
+    		"   gl_Position = position;\n"
+    		"}\n";
+
+    std::string fragmentShader =
+    		"#version 330 core \n"
+    		"\n"
+    		"layout(location=0) out vec4 color;"
+    		"\n"
+    		"void main()\n"
+    		"{\n"
+    		"    color = vec4(1.0, 0.0, 0.0, 1.0);\n"
+    		"}\n";
+    unsigned int shader = createShader(vertexShader, fragmentShader);
+    glUseProgram(shader);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
@@ -57,7 +116,7 @@ int main(void)
         /* Poll for and process events */
         glfwPollEvents();
     }
-
+    glDeleteProgram(shader);
     glfwTerminate();
     return 0;
 }
