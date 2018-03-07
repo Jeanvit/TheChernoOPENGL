fdec58ad2930ca97d4f7f2d89ce4cef69d813c9f
Jean <jeanvitordepaulo@gmail.com>
2018-03-07 20:19:29 -0300

    code of Projection Matrices in OpenGL video

--- src/Application.cpp
+++ src/Application.cpp
@@ -48,10 +48,10 @@ int main(void)
     }
 
     {
-		float positions[]={-0.5f,-0.5f, 0.0f, 0.0f,//0
-							0.5f,-0.5f, 1.0f, 0.0f,//1
-							0.5f, 0.5f, 1.0f, 1.0f,//2
-							-0.5f,0.5f, 0.0f, 1.0f //3
+		float positions[]={100.0f,100.0f, 0.0f, 0.0f,//0
+						   200.0f,100.0f, 1.0f, 0.0f,//1
+						   200.0f,200.0f, 1.0f, 1.0f,//2
+						   100.0f,200.0f, 0.0f, 1.0f //3
 		};
 
 		unsigned int indices[] ={
@@ -72,7 +72,12 @@ int main(void)
 
 		IndexBuffer ib(indices,6);
 
-		glm::mat4 proj = glm::ortho(-2.0f, 2.0f, -1.5f, 1.5f, -1.0f, 1.0f);
+		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+
+		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
+
+		glm::vec4 result = vp * proj ;
+
 		Shader shader("res/shaders/basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
