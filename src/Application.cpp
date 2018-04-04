3e0e8cf3db0b8f5e712f68e39ac57d629c36c43b
Jean <jeanvitordepaulo@gmail.com>
2018-04-04 20:33:08 -0300

    code of Model View Projection Matrices in OpenGL video

--- src/Application.cpp
+++ src/Application.cpp
@@ -73,15 +73,18 @@ int main(void)
 		IndexBuffer ib(indices,6);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100,0,0));
+		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200,200,0));
+
+		glm::mat4 mvp = proj * view * model;
 
 		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
 
-		glm::vec4 result = vp * proj ;
 
 		Shader shader("res/shaders/basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", proj );
+		shader.SetUniformMat4f("u_MVP", mvp );
 
 		Texture texture("res/textures/jeanlogo.png");
 		texture.Bind();
