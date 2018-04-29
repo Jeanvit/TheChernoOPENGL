a2ea72f639e4b0baff93047ddc31c542f8e4b680
Jean <jeanvitordepaulo@gmail.com>
2018-04-29 16:04:11 -0300

    code of ImGui in OpenGL video

--- src/Application.cpp
+++ src/Application.cpp
@@ -15,6 +15,8 @@
 
 #include "glm/glm.hpp"
 #include "glm/gtc/matrix_transform.hpp"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw_gl3.h"
 
 using namespace std;
 
@@ -74,9 +76,7 @@ int main(void)
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
 		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100,0,0));
-		glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(200,200,0));
 
-		glm::mat4 mvp = proj * view * model;
 
 		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
 
@@ -84,7 +84,8 @@ int main(void)
 		Shader shader("res/shaders/basic.shader");
 		shader.Bind();
 		shader.SetUniform4f("u_Color", 0.8f, 0.3f, 0.8f, 1.0f);
-		shader.SetUniformMat4f("u_MVP", mvp );
+
+
 
 		Texture texture("res/textures/jeanlogo.png");
 		texture.Bind();
@@ -97,14 +98,24 @@ int main(void)
 
 		Renderer renderer;
 
+		ImGui::CreateContext();
+		ImGui_ImplGlfwGL3_Init(window, true);
+		ImGui::StyleColorsDark();
+		glm::vec3 translation(200,200,0);
 		float r = 0.0f;
 		float increment = 0.05f;
 		while (!glfwWindowShouldClose(window))
 		{
 			/* Render here */
 			renderer.Clear();
+			ImGui_ImplGlfwGL3_NewFrame();
+
+			glm::mat4 model = glm::translate(glm::mat4(1.0f),translation );
+			glm::mat4 mvp = proj * view * model;
+
 			shader.Bind();
 			shader.SetUniform4f("u_Color",  r, 0.3f, 0.8f, 1.0f);
+			shader.SetUniformMat4f("u_MVP", mvp );
 
 			renderer.Draw(va,ib,shader);
 			if (r> 1.0f)
@@ -114,10 +125,17 @@ int main(void)
 
 			r +=increment;
 
+			{
+			           ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
+			           ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			        }
+
 			GLCall(glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT, nullptr));
 
 
 			/* Swap front and back buffers */
+			ImGui::Render();
+			ImGui_ImplGlfwGL3_RenderDrawData(ImGui::GetDrawData());
 			GLCall(glfwSwapBuffers(window));
 
 			/* Poll for and process events */
@@ -125,7 +143,8 @@ int main(void)
 		}
 
 	}
-
+    ImGui_ImplGlfwGL3_Shutdown();
+    ImGui::DestroyContext();
     glfwTerminate();
     return 0;
 }
