68a77b6da82e640cd680cfd5df8370d04185ed92
Jean <jeanvitordepaulo@gmail.com>
2018-05-13 14:01:38 -0300

    code of Rendering Multiple Objects in OpenGL video

--- src/Application.cpp
+++ src/Application.cpp
@@ -33,7 +33,7 @@ int main(void)
     glfwWindowHint(GLFW_OPENGL_PROFILE,GLFW_OPENGL_CORE_PROFILE);
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "TheCherno OpenGL - Implementation by Jean Vitor (www.jeanvitor.com)", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -50,10 +50,10 @@ int main(void)
     }
 
     {
-		float positions[]={100.0f,100.0f, 0.0f, 0.0f,//0
-						   200.0f,100.0f, 1.0f, 0.0f,//1
-						   200.0f,200.0f, 1.0f, 1.0f,//2
-						   100.0f,200.0f, 0.0f, 1.0f //3
+		float positions[]={-50.0f,-50.0f, 0.0f, 0.0f,//0
+						    50.0f,-50.0f, 1.0f, 0.0f,//1
+						    50.0f, 50.0f, 1.0f, 1.0f,//2
+						   -50.0f, 50.0f, 0.0f, 1.0f //3
 		};
 
 		unsigned int indices[] ={
@@ -75,7 +75,7 @@ int main(void)
 		IndexBuffer ib(indices,6);
 
 		glm::mat4 proj = glm::ortho(0.0f, 960.0f, 0.0f, 540.0f, -1.0f, 1.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(-100,0,0));
+		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0,0,0));
 
 
 		glm::vec4 vp(100.0f, 100.0f, 0.0f, 1.0f);
@@ -101,7 +101,10 @@ int main(void)
 		ImGui::CreateContext();
 		ImGui_ImplGlfwGL3_Init(window, true);
 		ImGui::StyleColorsDark();
-		glm::vec3 translation(200,200,0);
+
+		glm::vec3 translationA(200,200,0);
+		glm::vec3 translationB(400,200,0);
+
 		float r = 0.0f;
 		float increment = 0.05f;
 		while (!glfwWindowShouldClose(window))
@@ -110,14 +113,23 @@ int main(void)
 			renderer.Clear();
 			ImGui_ImplGlfwGL3_NewFrame();
 
-			glm::mat4 model = glm::translate(glm::mat4(1.0f),translation );
-			glm::mat4 mvp = proj * view * model;
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f),translationA );
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp );
+				renderer.Draw(va,ib,shader);
+			}
 
-			shader.Bind();
-			shader.SetUniform4f("u_Color",  r, 0.3f, 0.8f, 1.0f);
-			shader.SetUniformMat4f("u_MVP", mvp );
 
-			renderer.Draw(va,ib,shader);
+			{
+				glm::mat4 model = glm::translate(glm::mat4(1.0f),translationB );
+				glm::mat4 mvp = proj * view * model;
+				shader.Bind();
+				shader.SetUniformMat4f("u_MVP", mvp );
+				renderer.Draw(va,ib,shader);
+			}
+
 			if (r> 1.0f)
 				increment = -0.05f;
 			else if (r< 0.0f)
@@ -126,9 +138,10 @@ int main(void)
 			r +=increment;
 
 			{
-			           ImGui::SliderFloat3("Translation", &translation.x, 0.0f, 960.0f);
-			           ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
-			        }
+			    ImGui::SliderFloat3("Translation A", &translationA.x, 0.0f, 960.0f);
+			    ImGui::SliderFloat3("Translation B", &translationB.x, 0.0f, 960.0f);
+			    ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+			}
 
 			GLCall(glDrawElements(GL_TRIANGLES,6, GL_UNSIGNED_INT, nullptr));
 
